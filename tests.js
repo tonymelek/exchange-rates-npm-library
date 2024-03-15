c3fa1c604f83fffc17972e69d672a812ec9e1973
Tony Melek <tonymelek.au@gmail.com>
2024-03-16 10:45:33 +1100

    tests

---
+++ tests.js
@@ -0,0 +1,10 @@
+const { aussie, egy } = require('./index');
+const dotenv = require('dotenv');
+dotenv.config();
+console.log(process.argv[2]);
+const tests = async () => {
+    console.log(process.env.NAME)
+    console.log('testing aussie function output', await aussie());
+    console.log('testing egy function output', await egy());
+}
+// tests();
